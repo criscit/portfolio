ec5941b183afa19dbf92f82fe3b3abfe0dae1015
criscit15 <criscit15@mail.ru>
2022-05-21 11:19:00 +0300

    'added'

--- SQL/Social nework case/Queries.sql
+++ SQL/Social nework case/Queries.sql
@@ -1,11 +1,6 @@
-/* Нужен JOIN
+/*Определить кто больше поставил лайков (всего): мужчины или женщины.
+С использованием JOIN*/
 
-Определить кто больше поставил лайков (всего) - мужчины или женщины?
-
-Найти 10 пользователей, которые проявляют наименьшую активность в использовании
-социальной сети
-
-WITH likes_from_gender AS(
 WITH likes AS(
 SELECT COUNT(from_user_id) AS total, 
 from_user_id AS hit_like FROM like_to_user
@@ -18,14 +13,45 @@ UNION ALL
 SELECT COUNT(from_user_id) AS total, 
 from_user_id AS hit_like FROM like_to_post
 GROUP BY hit_like)
-SELECT hit_like, SUM(total) AS number_of_likes FROM likes 
-GROUP BY hit_like 
-ORDER BY number_of_likes DESC)
-SELECT users.gender FROM users WHERE id IN (
-SELECT hit_like FROM likes_from_gender) 
-GROUP BY gender;
+SELECT u.gender, SUM(total) AS number_of_likes FROM likes
+INNER JOiN users u
+ON likes.hit_like = u.id
+GROUP BY gender ORDER BY number_of_likes DESC LIMIT 1;
 
-216*/
+
+/*Подсчитать общее количество лайков, которые получили пользователи младше 10 лет.
+С использованием JOIN*/
+
+SELECT COUNT(to_user_id) AS number_of_likes, u.*, 
+FLOOR((to_days(NOW())-to_days(u.birthday))/365.25) AS age FROM like_to_user l 
+INNER JOIN users u
+ON u.id = l.to_user_id
+WHERE FLOOR((to_days(NOW())-to_days(u.birthday))/365.25)<10
+GROUP BY to_user_id ORDER BY age;
+
+/*Пусть задан некоторый пользователь. Из всех пользователей соц. сети найдите человека, 
+который больше всех общался с выбранным пользователем (написал ему сообщений).
+С использованием JOIN*/
+
+SELECT u.*, m.to_user_id, COUNT(m.from_user_id) AS number_of_messages FROM users u
+INNER JOIN messages m
+ON m.from_user_id = u.id AND m.to_user_id = 2 GROUP BY m.from_user_id ORDER BY number_of_messages DESC LIMIT 1;
+
+/* Определить кто больше поставил лайков (всего) - мужчины или женщины?*/
+
+WITH likes AS(
+SELECT COUNT(from_user_id) AS total, 
+from_user_id AS hit_like, (SELECT gender FROM users WHERE id=like_to_user.from_user_id) AS gender FROM like_to_user
+GROUP BY hit_like
+UNION ALL
+SELECT COUNT(from_user_id) AS total, 
+from_user_id AS hit_like, (SELECT gender FROM users WHERE id=like_to_media.from_user_id) AS gender FROM like_to_media
+GROUP BY hit_like
+UNION ALL
+SELECT COUNT(from_user_id) AS total, 
+from_user_id AS hit_like, (SELECT gender FROM users WHERE id=like_to_post.from_user_id) AS gender FROM like_to_post
+GROUP BY hit_like)
+SELECT gender, SUM(total) AS number_of_likes FROM likes GROUP BY gender ORDER BY number_of_likes DESC LIMIT 1;
 
 /*Пусть задан некоторый пользователь.
 Из всех друзей этого пользователя найдите человека, который больше всех общался с нашим
